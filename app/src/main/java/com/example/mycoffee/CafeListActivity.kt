c9054b97e798eadd45bc1c824db02b46a5b1a8bd
serhat samioglu <serhatsamioglu35@gmail.com>
2022-01-09 15:09:52 +0300

    optionsMenu done

--- app/src/main/java/com/example/mycoffee/CafeListActivity.kt
+++ app/src/main/java/com/example/mycoffee/CafeListActivity.kt
@@ -2,10 +2,27 @@ package com.example.mycoffee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import com.example.mycoffee.services.Firebase
 
 class CafeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cafe_list)
+        setTitle("Hoşgeldin Serhat") // todo: base' e taşınabilir
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Firebase.signOutUser()
+        Toast.makeText(this, "Option Pressed", Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
     }
 }
