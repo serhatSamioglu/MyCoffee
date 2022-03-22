10093bd247b269335af705bed2f75cc23e475fb1
serhat samioglu <serhatsamioglu35@gmail.com>
2022-03-23 00:03:22 +0300

    firebase crashlytics added

--- app/src/main/java/com/example/mycoffee/activitylist/ActivityListActivity.kt
+++ app/src/main/java/com/example/mycoffee/activitylist/ActivityListActivity.kt
@@ -3,11 +3,16 @@ package com.example.mycoffee.activitylist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mycoffee.R
+import com.example.mycoffee.authentication.AuthenticationActivity
 import com.example.mycoffee.databinding.ActivityListBinding
 import com.example.mycoffee.employeelist.EmployeeListActivity
+import com.example.mycoffee.services.Firebase
 import kotlinx.coroutines.flow.collectLatest
 
 class ActivityListActivity : AppCompatActivity() {
@@ -70,5 +75,17 @@ class ActivityListActivity : AppCompatActivity() {
     private fun navigateNewScreen(intent: Intent) {
         startActivity(intent)
         // finish() // Todo: BaseActivitye taşındığında finish() olayı parametre ile kontrol edilebilir
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Firebase.signOutUser()
+        navigateNewScreen(Intent(applicationContext, AuthenticationActivity::class.java))
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/mycoffee/employeelist/EmployeeListActivity.kt
+++ app/src/main/java/com/example/mycoffee/employeelist/EmployeeListActivity.kt
@@ -3,17 +3,13 @@ package com.example.mycoffee.employeelist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mycoffee.R
-import com.example.mycoffee.authentication.AuthenticationActivity
 import com.example.mycoffee.databinding.ActivityEmployeeListBinding
-import com.example.mycoffee.services.Firebase
 import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.coroutines.flow.collectLatest
 import org.json.JSONException
@@ -109,22 +105,5 @@ class EmployeeListActivity : AppCompatActivity() {
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.nav_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Firebase.signOutUser()
-        navigateNewScreen(Intent(applicationContext, AuthenticationActivity::class.java))
-        finish()
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun navigateNewScreen(intent: Intent) {
-        startActivity(intent)
-        // finish() // Todo: BaseActivitye taşındığında finish() olayı parametre ile kontrol edilebilir
     }
 }
